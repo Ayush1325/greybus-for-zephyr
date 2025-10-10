e7bce0a5feb443be54ae86fa5154f87e2bc33855
Ayush Singh <ayush@beagleboard.org>
2025-10-10 23:04:14 +0530

    Remove timesync stubs
    - Remove unimplemented timesync stubs.
    - Add todo to properly implement it someday. Not sure if the transport
      latency makes it even possible.
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- subsys/greybus/control-gpb.c
+++ subsys/greybus/control-gpb.c
@@ -32,7 +32,6 @@
 #include <zephyr/sys/byteorder.h>
 #include <greybus/greybus.h>
 #include <zephyr/device.h>
-#include <greybus/timesync.h>
 #include <greybus-utils/manifest.h>
 #include "greybus_messages.h"
 #include "greybus_transport.h"
@@ -155,73 +154,6 @@ static void gb_control_interface_version(uint16_t cport, struct gb_message *req)
 	gb_transport_message_response_success_send(req, &resp_data, sizeof(resp_data), cport);
 }
 
-static void gb_control_timesync_enable(uint16_t cport, struct gb_message *req)
-{
-	uint8_t count;
-	uint64_t frame_time;
-	uint32_t strobe_delay;
-	uint32_t refclk;
-	const struct gb_control_timesync_enable_request *req_data =
-		(const struct gb_control_timesync_enable_request *)req->payload;
-	int retval;
-
-	if (gb_message_payload_len(req) < sizeof(*req_data)) {
-		LOG_ERR("dropping short message");
-		return gb_transport_message_empty_response_send(req, GB_OP_INVALID, cport);
-	}
-
-	count = req_data->count;
-	frame_time = sys_le64_to_cpu(req_data->frame_time);
-	strobe_delay = sys_le32_to_cpu(req_data->strobe_delay);
-	refclk = sys_le32_to_cpu(req_data->refclk);
-
-	retval = timesync_enable(count, frame_time, strobe_delay, refclk);
-	gb_transport_message_empty_response_send(req, gb_errno_to_op_result(retval), cport);
-}
-
-static void gb_control_timesync_disable(uint16_t cport, struct gb_message *req)
-{
-	int retval = timesync_disable();
-	gb_transport_message_empty_response_send(req, gb_errno_to_op_result(retval), cport);
-}
-
-static void gb_control_timesync_authoritative(uint16_t cport, struct gb_message *req)
-{
-	uint64_t frame_time[GB_TIMESYNC_MAX_STROBES];
-	const struct gb_control_timesync_authoritative_request *req_data =
-		(const struct gb_control_timesync_authoritative_request *)req->payload;
-	int i;
-	int retval;
-
-	if (gb_message_payload_len(req) < sizeof(*req_data)) {
-		LOG_ERR("dropping short message");
-		return gb_transport_message_empty_response_send(req, GB_OP_INVALID, cport);
-	}
-
-	for (i = 0; i < GB_TIMESYNC_MAX_STROBES; i++) {
-		frame_time[i] = sys_le64_to_cpu(req_data->frame_time[i]);
-	}
-
-	retval = timesync_authoritative(frame_time);
-	gb_transport_message_empty_response_send(req, gb_errno_to_op_result(retval), cport);
-}
-
-static void gb_control_timesync_get_last_event(uint16_t cport, struct gb_message *req)
-{
-	uint64_t frame_time;
-	struct gb_control_timesync_get_last_event_response resp_data;
-	int retval;
-
-	retval = timesync_get_last_event(&frame_time);
-	if (!retval) {
-		resp_data.frame_time = sys_cpu_to_le64(frame_time);
-		return gb_transport_message_response_success_send(req, &resp_data,
-								  sizeof(resp_data), cport);
-	}
-
-	gb_transport_message_empty_response_send(req, gb_errno_to_op_result(retval), cport);
-}
-
 static void gb_control_handler(const void *priv, struct gb_message *msg, uint16_t cport)
 {
 	ARG_UNUSED(priv);
@@ -251,14 +183,12 @@ static void gb_control_handler(const void *priv, struct gb_message *msg, uint16_
 	/* XXX SW-4136: see control-gb.h */
 	/*GB_HANDLER(GB_CONTROL_TYPE_INTF_POWER_STATE_SET, gb_control_intf_pwr_set),
 	GB_HANDLER(GB_CONTROL_TYPE_BUNDLE_POWER_STATE_SET, gb_control_bundle_pwr_set),*/
+	/* TODO: Properly implement timesync */
 	case GB_CONTROL_TYPE_TIMESYNC_ENABLE:
-		return gb_control_timesync_enable(cport, msg);
 	case GB_CONTROL_TYPE_TIMESYNC_DISABLE:
-		return gb_control_timesync_disable(cport, msg);
 	case GB_CONTROL_TYPE_TIMESYNC_AUTHORITATIVE:
-		return gb_control_timesync_authoritative(cport, msg);
 	case GB_CONTROL_TYPE_TIMESYNC_GET_LAST_EVENT:
-		return gb_control_timesync_get_last_event(cport, msg);
+		return gb_transport_message_empty_response_send(msg, GB_OP_SUCCESS, cport);
 	default:
 		LOG_ERR("Invalid type");
 		gb_transport_message_empty_response_send(msg, GB_OP_INVALID, cport);
--- subsys/greybus/greybus-stubs.h
+++
@@ -1,9 +0,0 @@
-#ifndef __GREYBUS_STUBS_H__
-#define __GREYBUS_STUBS_H__
-
-void timesync_enable();
-void timesync_disable();
-void timesync_authoritative();
-void timesync_get_last_event();
-
-#endif /* __GREYBUS_STUBS_H__ */
