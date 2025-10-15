0f8711b98c0075466f39f43573f487b347dd21fb
Ayush Singh <ayush@beagleboard.org>
2025-10-15 17:50:23 +0530

    subsys: greybus: Allow SPI to build
    - Not actually tested if it works.
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

---
+++ subsys/greybus/greybus_spi.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2025 Ayush Singh BeagleBoard.org
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _GREYBUS_SPI_H_
+#define _GREYBUS_SPI_H_
+
+#include <stdint.h>
+#include "spi-gb.h"
+
+struct gb_spi_device_data {
+	struct gb_spi_device_config_response data;
+	uint8_t id;
+};
+
+struct gb_spi_driver_data {
+	const struct gb_spi_device_data *devices;
+	const struct device *dev;
+	uint8_t device_num;
+};
+
+#endif // _GREYBUS_SPI_H_
